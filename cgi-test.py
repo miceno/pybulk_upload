61dda6c86140abe51952c9713f7edb4e3e030ade
miceno <orestes@acm.org>
2012-03-01 01:12:28 +0100

    NEW: script for testing CGI

--- cgi-test.py
+++ cgi-test.py
@@ -1,13 +1,22 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 print "Content-type: text/html"
 print
-print "<html><head><title>Situation snapshot</title></head><body><pre>" import sys
+print "<html><head><title>Situation snapshot</title></head><body><pre>" 
+import sys
 sys.stderr = sys.stdout
 import os
+import cgi
 from cgi import escape
+import cgitb
+cgitb.enable()
 print "<strong>Python %s</strong>" % sys.version
 keys = os.environ.keys( )
 keys.sort( )
 for k in keys:
     print "%s\t%s" % (escape(k), escape(os.environ[k])) 
+
+form = cgi.FieldStorage()
+for i in form:
+    print "%s: %s" % ( i, form.getvalue( i ) )
+    
 print "</pre></body></html>"
