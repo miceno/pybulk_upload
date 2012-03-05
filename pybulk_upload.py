f595c59aa002cc15d6a4adb359c5960e83433cc2
miceno <orestes@acm.org>
2012-03-05 01:23:32 +0100

    First commit of summarize function

--- pybulk_upload.py
+++ pybulk_upload.py
@@ -18,6 +18,7 @@ import logging
 import tempfile
 import collections
 import cgi
+import re
 
 # Debug trace for CGI
 import cgitb
@@ -28,7 +29,17 @@ cgitb.enable()
 tempdir = tempfile.gettempdir()
 log_file=os.path.join( tempdir, os.path.splitext( sys.argv[0] )[0]+ ".log" )
 logging.basicConfig(filename=log_file,level=logging.DEBUG)
-   
+
+stop_delimiters = re.compile( '[(\.,;\[]|\si\s')
+MAX_SUMMARY = 100
+def summarize( message ):
+    # Position for first closing delimiter 
+    result = message[:MAX_SUMMARY]
+    match = stop_delimiters.search( message )
+    if match is not None and match.start() < MAX_SUMMARY:
+        result = message[:match.start()]
+    return result
+
 class HtmlFormatter:
     def __init__ ( self ):
         pass
@@ -99,7 +110,7 @@ class BulkOperationFormatter:
         result = []
         # Strip extention of the filename
         result.append( os.path.splitext( os.path.split( file_name )[1] )[0] )
-        result.append( row[ self.DESCRIPTION ].value[:self.MAX_SUMMARY] )
+        result.append( summarize( row[ self.DESCRIPTION ].value) )
         result.append( row[ self.DESCRIPTION ].value )
         keywords = []
         keywords.append( str( int( row[ self.NUMBER ].value ) ) )
@@ -290,5 +301,87 @@ def main(argv=None):
         pass
     write_response( headers, response )
     
-if __name__ == "__main__":
+def test_main():
     sys.exit(main())
+    
+def test_summary():
+    texto = ( \
+    """Visita a la Torre de les Aigües (Pere Falqués, 1881) rehabilitada per l'arquitecte Antoni Vilanova (centre). A la dreta XXXX, arquitecte de l'equip de Vilanova. A l'esquerra, Jordi Fossas, arquitecte i president de l'AHPN.""", \
+    """Visita a la Torre de les Aigües (Pere Falqués, 1881) rehabilitada per l'arquitecte Antoni Vilanova (esquerra). Cap a la dreta Jordi Fossas, arquitecte i president de l'AHPN i Juan Roca, del MUHBA.""", \
+    """Visita a la Torre de les Aigües (Pere Falqués, 1881). A l'esquerra, Juan Roca del MUHBA, en el centre el representant d'Aigües de Barcelona. Cap a la dreta, Xavier Pegenaute i Salvador Clarós, president i expresident de l'AVPN.""", \
+    """Làpida a l'església del Sagrat Cor de Jesús, de la sepultura on varen ser traslladades el 5/3/1928 les restes dels germans Laureano Arango Portús (mort el 23/6/1891) i Josefa Arango Portús (morta el 4/1/1918), fundadors de la parròquia per disposició dels seus testaments. El 1936 la tomba fou profanada i les restes cremades al carrer. El que en quedà fou sepultat en un nínxol al cementiri de l'est, però per falta de pagament l'ajuntament el buidà i les restes acabaren a la fossa comú.""", \
+    """Làpida a l'església del Sagrat Cor de Jesús, de la sepultura on varen ser traslladades el 5/3/1928 les restes dels germans Laureano Arango Portús (mort el 23/6/1891) i Josefa Arango Portús (morta el 4/1/1918), fundadors de la parròquia per disposició dels seus testaments. El 1936 la tomba fou profanada i les restes cremades al carrer. El que en quedà fou sepultat en un nínxol al cementiri de l'est, però per falta de pagament l'ajuntament el buidà i les restes acabaren a la fossa comú.""", \
+    """Interior de l'Església del Sagrat Cor de Jesús, la única del Poblenou que nou fou totalment destruïda pel foc el 1936, gràcies als treballadors de les cotxeres del costat que van tenir por que el foc es propagués. En el terra s'observen els senyals del foc, que va enderrocar la coberta.""", \
+    """Interior de l'Església del Sagrat Cor de Jesús, la única del Poblenou que nou fou totalment destruïda pel foc el 1936, gràcies als treballadors de les cotxeres del costat que van tenir por que el foc es propagués. En el terra s'observen els senyals del foc, que va enderrocar la coberta.""", \
+    """Interior de l'Església del Sagrat Cor de Jesús, la única del Poblenou que nou fou totalment destruïda pel foc el 1936, gràcies als treballadors de les cotxeres del costat que van tenir por que el foc es propagués. En el terra s'observen els senyals del foc, que va enderrocar la coberta.""", \
+    """Interior de l'Església del Sagrat Cor de Jesús, la única del Poblenou que nou fou totalment destruïda pel foc el 1936, gràcies als treballadors de les cotxeres del costat que van tenir por que el foc es propagués. En el terra s'observen els senyals del foc, que va enderrocar la coberta.""", \
+    """Anunci, esquela a La Vanguardia pel trasllat el 5/3/1928, a l'església del Sagrat Cor de Jesús, de les restes dels germans Laureano Arango Portús (mort el 23/6/1891) i Josefa Arango Portús (morta el 4/1/1918), fundadors de la parròquia per disposició dels seus testaments.""", \
+    """Anunci, invitació a la inauguració i benedicció de l'església del Sagrat Cor de Jesús, el 10/6/1926.""", \
+    """Façana de l'Església del Sagrat Cor de Jesús""", \
+    """Ceràmica d'Olivé Milian, 2002, dedicada a la Mare de Deu de Montserrat, damunt l'entrada a la façana de l'Església del Sagrat Cor de Jesús""", \
+    """Pintada de la Guerra 1936-1939 "Viva la FAI y CNT" a l'interior del campanar de l'Església del Sagrat Cor de Jesús. També es poden llegir pintades obcenes com "El cura de esta casa de putas..." o "El cura de esta misa es un maricón...""", \
+    """Pintada de la Guerra 1936-1939 "Madrid es nuestro, de las fuerzas leales a la república, y lo será siempre""", \
+    """Pintada de la Guerra 1936-1939, nu femení amb frases obcenes.""", \
+    """Única campana conservada, la "Isidra" a l'Església del Sagrat Cor de Jesús. La campana porta els noms de Martina, Isidra i Antònia, i la data de 15/5/19XX""", \
+    """Des del campanar de l'Església del Sagrat Cor, la fàbrica cremada de Ca l'Alier al carrer Fluvià-Talsa. A la dreta, Pere IV.""", \
+    """Des del campanar de l'Església del Sagrat Cor, la fàbrica cremada de Ca l'Alier al carrer Fluvià-Talsa.""", \
+    """Des del campanar de l'Església del Sagrat Cor, el carrer Pere IV. Al fons, la torre AGBAR.""", \
+    """Des del campanar de l'Església del Sagrat Cor, panoràmica cap al Besòs. Al fons, les xemeneies, fora d'ús, de la incineradora del Besòs.""", \
+    """Des del campanar de l'Església del Sagrat Cor, panoràmica cap al mar. Al fons, la xemeneia de MACOSA i els gratacels de Diagonal Mar. Els camps de la dreta són els únics del Poblenou que no han estat mai edificats.""", \
+    """Antics esgrafiats han aparegut sota l'arrebossat de la finca Lacambra""", \
+    """Antics esgrafiats han aparegut sota l'arrebossat de la finca Lacambra""", \
+    """Hotel ME. Des d'una porta del Parc del Centre, Pere IV, enfront, talla la Diagonal i resta trossejat pel parc.""", \
+    """El Poblenou sense gas. L'entrada d'aigua a la xarxa de gas a Pujades 196 obliga a tallar-ne el subministrament a uns 15.000 habitatges des de l'11 al 16/2/2012. Per a reparar-ho Gas Natural va obrir 250 rases, des de Taulat fins a Pere IV.""", \
+    """El Poblenou sense gas. L'entrada d'aigua a la xarxa de gas a Pujades 196 obliga a tallar-ne el subministrament a uns 15.000 habitatges des de l'11 al 16/2/2012. Per a reparar-ho Gas Natural va obrir 250 rases, des de Taulat fins a Pere IV.""", \
+    """El Poblenou sense gas. L'entrada d'aigua a la xarxa de gas a Pujades 196 obliga a tallar-ne el subministrament a uns 15.000 habitatges des de l'11 al 16/2/2012. Per a reparar-ho Gas Natural va obrir 250 rases, des de Taulat fins a Pere IV.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des d'un terrat del carrer Espronceda. Xemeneia. A l'esquerra habitatges en construcció al carrer Bolívia, al fons l'hotel ME. A la dreta la fàbrica Frigo-Farggi.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des d'un terrat del carrer Espronceda. Xemeneia. Pintada "Salvem Cant Ricart prou especulació". Al fons la fàbrica Frigo-Farggi.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. La torre des d'un terrat del carrer Espronceda. Al fons un tramvia a la Diagonal.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Base de la xemeneia des d'un terrat del carrer Espronceda.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des d'un terrat del carrer Espronceda.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Pintada contra el 22@ "Salvem Cant Ricart prou especulació".""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Xemeneia.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. La torre des d'un terrat del carrer Espronceda.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des d'un terrat del carrer Espronceda. Xemeneia. Al fons la fàbrica Frigo-Farggi.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des d'un terrat del carrer Espronceda. Pintada contra el 22@ "Salvem Cant Ricart prou especulació". Al fons la fàbrica Frigo-Farggi.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Pintada contra el 22@ "Salvem Cant Ricart prou especulació".""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Pintada "31 años aqui y no pagan un euro. Clos/Ricart= lladres""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Xemeneia i naus centrals.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus centrals amb estructura de ferro i fusta per a suportar el sostre de ceràmica.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus centrals amb estructura de ferro i fusta per a suportar el sostre de ceràmica.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. La xemeneia des de l'interior de les naus centrals.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. La xemeneia des de l'interior de les naus centrals.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des d'un terrat interior. Base de la xemeneia. Al fons l'hotel ME.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des d'un terrat interior. Habitatges en construcció al carrer Bolívia. A l'esquerra els habitatges d'Espronceda.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Interior de les naus centrals.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Acumulació d'enderrocs.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Edifici situat a l'entrada pel carrer Bolívia.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Singular edifici per les seves finestres i xemeneies.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Singular edifici per les seves finestres i xemeneies.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Pont de fusta cobert. Al fons l'hotel ME.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Pont de fusta cobert. Al fons la torre.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Singular edifici per les seves finestres i xemeneies.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des de l'edifici en construcció del carrer Bolívia. L'entrada, la plaça, el restaurant. Al fons el Parc Central i l'hotel ME.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des de l'edifici en construcció del carrer Bolívia. L'entrada, la plaça, el restaurant. Al fons el Parc Central i l'hotel ME.""", \
+    """Túnel soterrani a Can Ricart""", \
+    """Túnel soterrani a Can Ricart. Portes de ferro tancades.""", \
+    """Túnel soterrani a Can Ricart. Portes de ferro obertes.""", \
+    """Doc en pdf que recull l'acord entre l'alcalde Jordi Hereu amb el Conseller-Director General d’AGBAR, Àngel Simón, per a la rehabilitació, segons projecte d'Antoni Vilanova Omedas (1958-), de la Torre de les Aigües del Besòs i l’Antiga Casa de Vàlvules, construïts entre 1880 i 1882 segons projecte de Pere Falqués i Urpí (1850-1916).""", \
+    """Visita a la Torre de les Aigües (Pere Falqués, 1881) rehabilitada per l'arquitecte Antoni Vilanova (centre). A la dreta XXXX, arquitecte de l'equip de Vilanova. A l'esquerra, Jordi Fossas, arquitecte i president de l'AHPN.""", \
+    """Làpida a l'església del Sagrat Cor de Jesús, de la sepultura on varen ser traslladades el 5/3/1928 les restes dels germans Laureano Arango Portús (mort el 23/6/1891) i Josefa Arango Portús (morta el 4/1/1918), fundadors de la parròquia per disposició dels seus testaments. El 1936 la tomba fou profanada i les restes cremades al carrer. El que en quedà fou sepultat en un nínxol al cementiri de l'est, però per falta de pagament l'ajuntament el buidà i les restes acabaren a la fossa comú.""", \
+    """Interior de l'Església del Sagrat Cor de Jesús, la única del Poblenou que nou fou totalment destruïda pel foc el 1936, gràcies als treballadors de les cotxeres del costat que van tenir por que el foc es propagués. En el terra s'observen els senyals del foc, que va enderrocar la coberta.""", \
+    """Empitjora l'estat de Can Ricart després que les administracions aturessin Linguamón i altres projectes. Panoràmica des d'un terrat del carrer Espronceda. Xemeneia. A l'esquerra habitatges en construcció al carrer Bolívia, al fons l'hotel ME. A la dreta la fàbrica Frigo-Farggi."""
+    )
+    for t in texto:
+        print "resumen:", summarize( t )
+    
+if __name__ == "__main__":
+    test_summary( )
