006eabcee40de561f4531176df14112a90cfa6f4
miceno <orestes@acm.org>
2012-03-17 22:21:15 +0100

    Script to clean keywords

---
+++ clean_keywords.py
@@ -0,0 +1,34 @@
+
+
+import csv
+
+
+filename = 'g2_ChildEntity.csv'
+folders = {}
+
+def read_folders( filename ):
+    global folders
+    folders = {}
+    try:
+        for d in csv.DictReader(open(filename),fieldnames=['0','1']):
+            folders[ d['0'] ] = d['1']
+    except:
+        pass
+
+read_folders( 'carpetes.csv' )
+
+for i in csv.DictReader( open( filename ),delimiter=';',fieldnames=[ str(n) for n in xrange(0,3)] ):
+    keywords = i['2'].split( ',' )
+    keywords.insert( 0, i['0'] )
+    if len(keywords)> 2 and keywords[2] != '':
+        # Search for folder name
+        folder_number = keywords[1]
+        folder_name = folders.get( folder_number, "")
+        if folder_name != "":
+           keywords.append( folder_name )
+        # print k
+        s = "update g2_Item set g_keywords = '%s' where g_id = %s" % ( ",".join( [ unicode( keyword, 'utf-8') for keyword in keywords[1:] ] ), keywords[0] )
+        print s.encode( 'utf-8' )
+        
+        
+
