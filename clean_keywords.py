26197664e5cc6c1a8f489f0e858ede27563b6047
miceno <orestes@acm.org>
2012-03-18 01:09:46 +0100

    Missing semicolon at end of line, select the folder field, and escape single quotes

--- clean_keywords.py
+++ clean_keywords.py
@@ -17,17 +17,22 @@ def read_folders( filename ):
 
 read_folders( 'carpetes.csv' )
 
+database = 'db271970365'
+
+print "use %s;" % database
+print
+
 for i in csv.DictReader( open( filename ),delimiter=';',fieldnames=[ str(n) for n in xrange(0,3)] ):
     keywords = i['2'].split( ',' )
     keywords.insert( 0, i['0'] )
     if len(keywords)> 2 and keywords[2] != '':
         # Search for folder name
-        folder_number = keywords[1]
+        folder_number = keywords[2]
         folder_name = folders.get( folder_number, "")
         if folder_name != "":
            keywords.append( folder_name )
-        # print k
-        s = "update g2_Item set g_keywords = '%s' where g_id = %s" % ( ",".join( [ unicode( keyword, 'utf-8') for keyword in keywords[1:] ] ), keywords[0] )
+        # print keywords
+        s = "update g2_Item set g_keywords = '%s' where g_id = %s;" % ( ",".join( [ unicode( keyword, 'utf-8') for keyword in keywords[1:] ] ).replace( "'", r"\'"), keywords[0] )
         print s.encode( 'utf-8' )
         
         
